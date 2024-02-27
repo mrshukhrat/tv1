5af2214f410827b8c94d05a3c28d5610ef842db3
Shuxrat Begmatov <hp@mm50.uzbektourism.local>
2024-02-27 20:31:03 +0500

    fix

---
+++ precache-manifest.0fc551f6f15593c6604d3fd48fc91509.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14ef1218aca291e3045",
+    "url": "/css/app.8bcaa347.css"
+  },
+  {
+    "revision": "2726f59f4c58a091f2ce",
+    "url": "/css/chunk-vendors.071947a4.css"
+  },
+  {
+    "revision": "08c20a487911694291bd8c5de41315ad",
+    "url": "/fonts/Poppins-Bold.08c20a48.ttf"
+  },
+  {
+    "revision": "fcc40ae9a542d001971e53eaed948410",
+    "url": "/fonts/Poppins-Light.fcc40ae9.ttf"
+  },
+  {
+    "revision": "13a9b012e0ce631d874759eb5f702643",
+    "url": "/img/Facebook.13a9b012.svg"
+  },
+  {
+    "revision": "a4285be19cf2199e584c70754e8588b6",
+    "url": "/img/imagelogin.a4285be1.png"
+  },
+  {
+    "revision": "a60af3a220fac02bebc9e66181911022",
+    "url": "/img/logo.a60af3a2.svg"
+  },
+  {
+    "revision": "e096c7e0d4c29a3977486dcb198e5c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95457c107b64b935055",
+    "url": "/js/about.ef009237.js"
+  },
+  {
+    "revision": "b14ef1218aca291e3045",
+    "url": "/js/app.eae44740.js"
+  },
+  {
+    "revision": "2726f59f4c58a091f2ce",
+    "url": "/js/chunk-vendors.9cce0546.js"
+  },
+  {
+    "revision": "3ecefd356e0372e2679005494f13e250",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
